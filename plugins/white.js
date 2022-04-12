09eb363b628f19d30ec94b2d98b486a9d52d94a3
Fazbots <103348382+Fazbots@users.noreply.github.com>
2022-04-13 05:08:16 +0530

    Update white.js

--- plugins/white.js
+++ plugins/white.js
@@ -3,14 +3,14 @@
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 
-WHITE DEVIL ----»»» TERROR BOY
+FAZ ----»»» INRL
 */
 
 /*
  __________   _________     ________________  
      EDUTHO BUT CREDIT THERANAM
      
-     THANKS TO AFNAN-PLK 
+     THANKS TO FASWEEH KT 
 _______  __________  ___________  ________    
 */
 
